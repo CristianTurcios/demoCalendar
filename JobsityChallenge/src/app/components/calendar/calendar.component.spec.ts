bdb00dff992dcddcfecf206f0cd69384b5007a50
Andres Maltes <andmaltes@gmail.com>
2019-05-15 14:30:03 -0500

    Signed-off-by: Andres Maltes <andmaltes@gmail.com>

--- JobsityChallenge/src/app/components/calendar/calendar.component.spec.ts
+++
@@ -1,3 +0,0 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CalendarComponent } from './calendar.component';
--- JobsityChallenge/src/app/services/weather-forecast.service.spec.ts
+++
@@ -1,3 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-
-import { WeatherForecastService } from './weather-forecast.service';
